7c9a6dbd491a97d1caf2f96db3d83502d4ad3c29
i2gan <i2gun0f@gmail.com>
2023-02-13 17:22:22 +0400

    Lesson 18. Инкапсуляция. Сеттеры и геттеры.

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,19 +1,42 @@
 public class ClassesAndObjects {
     public static void main(String[] args) {
         Person person1 = new Person();
-        person1.setUserData("Roman", 27);
+        person1.setName("Romario");
+        person1.setAge(33);
+        System.out.println("Getting name in main method: " + person1.getName());
+        System.out.println("Getting age in main method: " + person1.getAge());
         person1.message();
     }
 }
 
 class Person {
-    String name;
-    int age;
+    private String name;
+    private int age;
 
-    void setUserData(String username, int userage) {
-        name = username;
-        age = userage;
+    public void setName(String userName) {
+        if (userName.isEmpty())
+            System.out.println("Enter Name");
+        else
+            name = userName;
     }
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int userAge) {
+        if (userAge < 0)
+            System.out.println("Age must be > 0");
+        else
+            age = userAge;
+    }
+    public int getAge() {
+        return age;
+    }
+
+//    void setUserData(String username, int userage) {
+//        name = username;
+//        age = userage;
+//    }
 
     void message() {
         System.out.println("My name is " + name + ", I'm " + age + " years old.");
