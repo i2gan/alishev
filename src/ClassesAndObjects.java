56d412b174b1cf47b90b2e495f0e76a1166018f4
i2gan <i2gun0f@gmail.com>
2023-02-13 13:34:46 +0400

    Lesson 17. Используем метод с параметрами

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,18 +1,8 @@
 public class ClassesAndObjects {
     public static void main(String[] args) {
         Person person1 = new Person();
-        person1.name = "Roman";
-        person1.age = 50;
-
-        Person person2 = new Person();
-        person2.name = "Vlad";
-        person2.age = 20;
-
-        int years1 = person1.calculateYearsToRetirement();
-        int years2 = person2.calculateYearsToRetirement();
-
-        System.out.println(years1 + " years for 1-st man to retirement");
-        System.out.println(years2 + " years for 2-nd man to retirement");
+        person1.setUserData("Roman", 27);
+        person1.message();
     }
 }
 
@@ -20,12 +10,12 @@ class Person {
     String name;
     int age;
 
-    int calculateYearsToRetirement() {
-        int years = 65 - age;
-        return years;
+    void setUserData(String username, int userage) {
+        name = username;
+        age = userage;
     }
 
     void message() {
-        System.out.println("My name " + name + " I'm " + age + " years old.");
+        System.out.println("My name is " + name + ", I'm " + age + " years old.");
     }
 }
