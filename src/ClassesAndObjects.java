b6077a507c2ebc626b18334c40a2fc73e5730638
i2gan <i2gun0f@gmail.com>
2023-02-13 13:31:27 +0400

    Add method with "int" type

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -3,18 +3,27 @@ public class ClassesAndObjects {
         Person person1 = new Person();
         person1.name = "Roman";
         person1.age = 50;
-        person1.message();
 
         Person person2 = new Person();
         person2.name = "Vlad";
         person2.age = 20;
-        person2.message();
+
+        int years1 = person1.calculateYearsToRetirement();
+        int years2 = person2.calculateYearsToRetirement();
+
+        System.out.println(years1 + " years for 1-st man to retirement");
+        System.out.println(years2 + " years for 2-nd man to retirement");
     }
 }
 
 class Person {
     String name;
     int age;
+
+    int calculateYearsToRetirement() {
+        int years = 65 - age;
+        return years;
+    }
 
     void message() {
         System.out.println("My name " + name + " I'm " + age + " years old.");
