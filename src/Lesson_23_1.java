4d33d1f2a6f78ece4ef5b9b530750a66813eca2e
i2gan <i2gun0f@gmail.com>
2023-02-14 22:05:54 +0400

    Lesson 23_1. Форматирование строк

---
+++ src/Lesson_23_1.java
@@ -0,0 +1,17 @@
+public class Lesson_23_1 {
+    public static void main(String[] args) {
+        System.out.printf("This is a string %s", "NICE");
+        System.out.printf("\nThis is a string %d", 10);
+        System.out.printf("\nThis is a string %10d", 123);
+        System.out.printf("\nThis is a string %-10d", 123);
+        System.out.printf("\nThis is a string %.2f", 3.1);
+
+        /*
+        This is a string NICE
+        This is a string 10
+        This is a string        123
+        This is a string 123
+        This is a string 3,10
+         */
+    }
+}
