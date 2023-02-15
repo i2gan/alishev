dc6289c023b8f837b06a33b537390b6d44286427
i2gan <i2gun0f@gmail.com>
2023-02-15 09:54:19 +0400

    Lesson 25. Наследование

---
+++ src/Animal.java
@@ -0,0 +1,10 @@
+public class Animal {
+
+    String name = "some animal";
+    public void eat() {
+        System.out.println("Animal is eating");
+    }
+    public void sleep() {
+        System.out.println("Animal is sleeping");
+    }
+}
---
+++ src/Dog.java
@@ -0,0 +1,17 @@
+public class Dog extends Animal {
+
+    public void eat() {
+        System.out.println("Dog is eating");
+    }
+
+    public void sleep() {
+        System.out.println("Dog is sleeping");
+    }
+    public void bark() {
+        System.out.println("Dog is barking");
+    }
+
+    public void showName() {
+        System.out.println(name);
+    }
+}
